78927b89cfe3f73c4916d166a64d44fa5918492d
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-01-26 15:33:40 -0800

    Initial Commit

---
+++ Veranda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Veranda
+//
+//  Created by Luis Cardenas on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Veranda/VerandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerandaApp.swift
+//  Veranda
+//
+//  Created by Luis Cardenas on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct VerandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
